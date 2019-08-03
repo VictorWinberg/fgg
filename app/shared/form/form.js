05aaacabed316951693798e3a97a8e6ae3b25cf6
Victor Winberg <victor.m.winberg@gmail.com>
2019-08-03 19:05:10 +0200

    §3. Tillagd dokumentuppladdning

--- app/shared/form/form.js
+++ app/shared/form/form.js
@@ -1,2 +1,57 @@
 angular.module('form', [])
-  .controller('FormController', function ($scope) {});
+  .controller('FormController', function ($scope) {
+    $scope.attendee = {
+      name: '',
+      position: ''
+    }
+    $scope.attendees = [];
+
+    $scope.addAttendee = function () {
+      $scope.attendees.push({
+        name: $scope.attendee.name,
+        position: $scope.attendee.position
+      });
+      $scope.attendee = {}
+    };
+    $scope.removeAttendee = function (attendee) {
+      $scope.attendees.splice($scope.attendees.indexOf(attendee), 1);
+    };
+
+    $scope.paragraph = {
+      title: '',
+      content: ''
+    }
+    $scope.paragraphs = [];
+    $scope.addParagraph = function () {
+      $scope.paragraphs.push({
+        title: $scope.paragraph.title,
+        content: $scope.paragraph.content
+      });
+      $scope.paragraph = {}
+    };
+    $scope.removeParagraph = function (paragraph) {
+      $scope.paragraphs.splice($scope.paragraphs.indexOf(paragraph), 1);
+    };
+
+    $scope.signature = {
+      comment: '',
+      name: '',
+      position: ''
+    }
+    $scope.signatures = [];
+    $scope.addSignature = function () {
+      $scope.signatures.push({
+        comment: $scope.signature.comment,
+        name: $scope.signature.name,
+        position: $scope.signature.position
+      });
+      $scope.signature = {}
+    };
+    $scope.removeSignature = function (signature) {
+      $scope.signatures.splice($scope.signatures.indexOf(signature), 1);
+    };
+
+    $scope.submit = function (form) {
+      console.log($("form").serializeArray());
+    };
+  });
